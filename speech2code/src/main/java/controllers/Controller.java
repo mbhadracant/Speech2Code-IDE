7f4eb778237c8d15e8c05f83d6c57cb7881fdcf2
mbhadracant <mbhadracant@gmail.com>
2016-12-09 15:26:44 +0000

    added basic speech recognition implementation

--- speech2code/src/main/java/controllers/Controller.java
+++ speech2code/src/main/java/controllers/Controller.java
@@ -5,7 +5,9 @@ import fxelements.JavaCodeArea;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import org.fxmisc.flowless.VirtualizedScrollPane;
+import speech_recognition.SpeechRecognizer;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,11 +18,21 @@ public class Controller implements Initializable {
 
     @FXML
     EditorStackPane editorStackPane;
+
     JavaCodeArea editor;
 
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View is now loaded!");
         VirtualizedScrollPane v = (VirtualizedScrollPane) editorStackPane.getChildren().get(0);
         editor = (JavaCodeArea) v.getContent();
+        Thread speechRecognizer = null;
+        try {
+            speechRecognizer = new Thread(new SpeechRecognizer(editor));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        speechRecognizer.start();
+
     }
 }
